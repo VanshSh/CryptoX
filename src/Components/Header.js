58c2e3fb2386b3f6cefa86241ca60281ff6e0246
Vansh Sharma <vanshsharma9354@gmail.com>
2022-04-18 13:56:43 +0530

    Style: Change the Header Styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,52 +1,47 @@
+import React from "react";
 import {
-    AppBar,
-    Container,
-    MenuItem,
-    Select,
-    Toolbar,
-    Typography,
-} from '@mui/material'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useCryptoContext } from '../Context'
+  Box,
+  AppBar,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useCryptoContext } from "../Context";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const { currency, setCurrency } = useCryptoContext()
+  const navigate = useNavigate();
+  const { currency, setCurrency } = useCryptoContext();
 
-    return (
-        <AppBar color='transparent' position='static' sx={{ padding: '1rem' }}>
-            <Container>
-                <Toolbar>
-                    <Typography
-                        variant='h4'
-                        onClick={() => navigate('/')}
-                        sx={{
-                            fontFamily:
-                                'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
-                            cursor: 'pointer',
-                        }}
-                    >
-                        Crypto<span className='title_span'>Pad</span>
-                    </Typography>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#050504" }}>
+        <Toolbar>
+          <Typography
+            component="div"
+            onClick={() => navigate("/")}
+            className="title"
+          >
+            CryptoX
+          </Typography>
 
-                    <Select
-                        labelId='demo-select-small'
-                        id='demo-select-small'
-                        variant='outlined'
-                        label='Currency'
-                        value={currency}
-                        className='select_currency'
-                        sx={{ color: 'white' }}
-                        onChange={(e) => setCurrency(e.target.value)}
-                    >
-                        <MenuItem value={'INR'}>INR</MenuItem>
-                        <MenuItem value={'USD'}>USD</MenuItem>
-                    </Select>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    )
-}
+          <Select
+            variant="outlined"
+            className="select_currency"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={currency}
+            sx={{ width: 100, height: 40, marginLeft: 15 }}
+            onChange={(e) => setCurrency(e.target.value)}
+          >
+            <MenuItem value={"INR"}>INR</MenuItem>
+            <MenuItem value={"USD"}>USD</MenuItem>
+          </Select>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
