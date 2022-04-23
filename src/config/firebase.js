e5d1b108d1b4170fc6f6f380c54e2624de0c7faf
VanshSh <vanshsharma9354@gmail.com>
2022-04-23 10:33:20 +0530

    refactor: :see_no_evil: Add the .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,18 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyCpRVZMOpC_ujrm5jEL94txfJx6z-daahA',
-    authDomain: 'cryptox-ad1ae.firebaseapp.com',
-    projectId: 'cryptox-ad1ae',
-    storageBucket: 'cryptox-ad1ae.appspot.com',
-    messagingSenderId: '963346833610',
-    appId: '1:963346833610:web:4c69670f067fb943a89433',
+    apiKey: process.env.REACT_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_FIREBASE_APP_ID,
+
+   
 }
 
 export default firebaseConfig
+//  apiKey: 'AIzaSyCpRVZMOpC_ujrm5jEL94txfJx6z-daahA',
+//     authDomain: 'cryptox-ad1ae.firebaseapp.com',
+//     projectId: 'cryptox-ad1ae',
+//     storageBucket: 'cryptox-ad1ae.appspot.com',
+//     messagingSenderId: '963346833610',
+//     appId: '1:963346833610:web:4c69670f067fb943a89433',
